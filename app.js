21138d3ef2ca40e597d3f120323cd7028b0a2ad8
Pablo de Andres <pablodeandresolano@gmail.com>
2022-07-19 17:08:30 +0200

    server config

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static("public"))
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+
+app.get("/home", (req, res ) => {
+
+    res.sendFile(__dirname + "/views/home.html")
+
+})
+
+app.get("/about", (req, res) => {
+    res.sendFile(__dirname + "/views/about.html")
+})
+
+app.get("/works", (req, res) => {
+    res.sendFile(__dirname + "/views/works.html")
+})
+
+
+app.get("*", (req, res) => {
+    res.send("Andeva jipicolgao??")
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
